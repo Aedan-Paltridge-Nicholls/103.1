6e766cdf7cdd81277d61234d2dba42f91bba24cd
Aedan H.P. Nicholls <99621280+Aedan-Paltridge-Nicholls@users.noreply.github.com>
2023-05-30 14:49:54 +1200

    Idk2

---
+++ task 5/build/libbgi/CMakeFiles/bgi.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/dibutil.cxx" "libbgi/CMakeFiles/bgi.dir/src/dibutil.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/dibutil.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/drawing.cxx" "libbgi/CMakeFiles/bgi.dir/src/drawing.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/drawing.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/file.cxx" "libbgi/CMakeFiles/bgi.dir/src/file.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/file.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/misc.cxx" "libbgi/CMakeFiles/bgi.dir/src/misc.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/misc.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/mouse.cxx" "libbgi/CMakeFiles/bgi.dir/src/mouse.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/mouse.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/palette.cxx" "libbgi/CMakeFiles/bgi.dir/src/palette.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/palette.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/text.cxx" "libbgi/CMakeFiles/bgi.dir/src/text.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/text.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/winbgi.cxx" "libbgi/CMakeFiles/bgi.dir/src/winbgi.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/winbgi.cxx.obj.d"
+  "D:/Users/270331389/source/repos/103.1/task 5/libbgi/src/winthread.cxx" "libbgi/CMakeFiles/bgi.dir/src/winthread.cxx.obj" "gcc" "libbgi/CMakeFiles/bgi.dir/src/winthread.cxx.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
