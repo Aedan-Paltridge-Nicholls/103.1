ba28f2a23c5274ef1944de81b541cbcc1fd34d8a
Aedan H.P. Nicholls <99621280+Aedan-Paltridge-Nicholls@users.noreply.github.com>
2023-06-02 11:35:18 +1200

    sfsf

--- Task 6/Tasksix.cpp
+++ Task 6/Tasksix.cpp
@@ -11,11 +11,11 @@
 •Manage relevant menu items to access the features. √
 */
 using namespace std;
-struct PersonalExpensesManager
+struct PersonalExpensesManager//This stores all of the expenses and dates.
 {
-    struct ExpenseDate
+    struct ExpenseDate//The date of an expense ,its value & type.
     {
-        struct Value
+        struct Value//The value & type of an expense.
         {
             struct expenses
             {
@@ -23,15 +23,15 @@ struct PersonalExpensesManager
             };
             expenses DailyExpenses;
         };
-        struct Days
+        struct Days//The day of week a expense was incurred.
         {
             Value Days[7];
         };
-        struct Week
+        struct Week//Which week of the month a expense was incurred.
         {
             Days Week[4];
         };
-        struct Month
+        struct Month//The Month of the year a expense was incurred.
         {
             Week Month[12];
         };
@@ -39,7 +39,7 @@ struct PersonalExpensesManager
     };
 
     ExpenseDate PersonalExpenses;
-    PersonalExpensesManager()
+    PersonalExpensesManager()//Constructor of PersonalExpensesManager that sets some initial values.
     {
 
         User.PersonalExpenses.Year.Month[1].Week[1].Days[1].DailyExpenses.EntertainmentCost = 100;
@@ -48,7 +48,7 @@ struct PersonalExpensesManager
         User.PersonalExpenses.Year.Month[1].Week[1].Days[1].DailyExpenses.TransportCosts = 12;
     }
 } User;
-void daysExpenses(int m, int w, int d)
+void daysExpenses(int m, int w, int d)//This displays the total expenses of a selected Day.
 {
     cout << "\nDaily expenses of Day : " << d << " of week : " << w << " of Month : " << m << "\n$"
      << User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.EntertainmentCost 
@@ -56,7 +56,7 @@ void daysExpenses(int m, int w, int d)
       + User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.MealCost 
       + User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.TransportCosts << endl;
 }
-void weeksExpenses(int m, int w)
+void weeksExpenses(int m, int w)//This displays the total sum expenses of a selected week.
 {
     int Cost = 0;
     cout << "\nDaily expenses of Week" << w << "-" << m << "\n $";
@@ -74,7 +74,7 @@ int main()
 {
     bool run = true;
     PersonalExpensesManager r1;
-    while (run == true)
+    while (run == true)//loops the program.
     {
         int input;
         cout <<"\nSee daily Expenses [1]\n"
@@ -85,7 +85,7 @@ int main()
         cin >> input;
         switch (input)
         {
-            case 1:
+            case 1://This let a user look at the total expenses of a selected day.
             {
                 int m, w, d;
                 cout << "\nFor what date\n"
@@ -95,10 +95,10 @@ int main()
                 cin >> w;
                 cout << "\nDay of the Week 1-7\n";
                 cin >> d;
-                daysExpenses(m, w, d);
+                daysExpenses(m, w, d);//calls the daysExpenses function and passes it the date of the day the user wants the total expenses of.
             }
             break;
-            case 2:
+            case 2://This let a user look at the total expenses of a selected week.
             {
                 int m, w, d;
 
@@ -107,10 +107,10 @@ int main()
                 cin >> m;
                 cout << "\nWeek of the Month 1-4\n";
                 cin >> w;
-                weeksExpenses(m,w);
+                weeksExpenses(m,w);//calls the weeksExpenses function and passes it the date of the week the user wants the total expenses of.
             }
             break;
-            case 3:
+            case 3://This lets a user add expenses at a user set date.
             {
                 bool escape = false;
                 int m, w, d, e;
@@ -122,7 +122,7 @@ int main()
                 cout << "\nDay of the Week 1-7\n";
                 cin >> d;
 
-                while (escape == false)
+                while (escape == false)//Loops so the user can add more than one expense to one day
                 {
                     e = 0;
                     cout << "what Expense\n"
@@ -131,54 +131,55 @@ int main()
                         << "\nMeal[3]"
                         << "\nTransport[4]"
                         << "\nStop Adding Expenses[5]\n";
-                    cin >> e;
-                    switch (e)
+                    cin >> e;// Holds the  users input.
+                    switch (e)// This lets a user set a value of an expense of a type corresponding to 'e'
                     {
-                        case 1:
+                        case 1:// This lets a user set a value of an expense of Entertainment Cost.
                         {
                             cout << "\nWhat Amount\n";
                             cin >> User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.EntertainmentCost;
                         }
                         break;
-                        case 2:
+                        case 2:// This lets a user set a value of an expense of Housing Cost.
                         {
                             cout << "\nWhat Amount\n";
                             cin >> User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.HousingCost;
                         }
                         break;
-                        case 3:
+                        case 3:// This lets a user set a value of an expense of Meal Cost.
                         {
                             cout << "\nWhat Amount\n";
                             cin >> User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.MealCost;
                         }
                         break;
-                        case 4:
+                        case 4:// This lets a user set a value of an expense of Transport Costs.
                         {
                             cout << "\nWhat Amount\n";
                             cin >> User.PersonalExpenses.Year.Month[m].Week[w].Days[d].DailyExpenses.TransportCosts;
                         }
                         break;
-                        case 5:
+                        case 5://This lets the user stop adding expenses.
                         {
                             escape = true;
                         }
                         break;
-
-                        default:
-                            break;
+                        default://This tells the the user if they try to enter an invalid input. 
+                        {
+                            cout <<"\n\nInvalid Input\nValid Inputs are 1-5\n\n";
+                        }
+                        break;
                     }
                 }
             }
             break;
-            case 4:
+            case 4://This ends the programe.
             {
                 run = false;
             }
             break;
-            default:
+            default://This tells the the user if they try to enter an invalid input. 
             {
                 cout <<"\n\nInvalid Input\nvalid inputs are 1-4\n\n";
-
             }
             break;
         }
